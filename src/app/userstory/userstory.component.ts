7c3d243380b4570b16fb5464d4a43d33e7139ca9
camkurup <camkurup@hotmail.com>
2022-11-24 09:28:10 +0100

    adjusted userstory.component.ts

--- src/app/userstory/userstory.component.ts
+++ src/app/userstory/userstory.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UserStory } from './userstory';
 
 @Component({
   selector: 'app-userstory',
   templateUrl: './userstory.component.html',
   styleUrls: ['./userstory.component.scss'],
 })
-export class UserstoryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class UserstoryComponent {
+  @Input() task: Task | null = null;
+  @Output() edit = new EventEmitter<Task>();
 }
