43c2f67a997f557d6e6fcc07a8d0f2dd46fa4159
camkurup <camkurup@hotmail.com>
2022-11-24 09:08:02 +0100

    Added Firebase to my project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,24 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5NPA579qcqa7OAgekCRwA-Z12UAI5_Ps",
+  authDomain: "scrumboardkurup.firebaseapp.com",
+  projectId: "scrumboardkurup",
+  storageBucket: "scrumboardkurup.appspot.com",
+  messagingSenderId: "199826615450",
+  appId: "1:199826615450:web:659236c803317e29a08323",
+  measurementId: "G-N74Q8MKCGF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
